522aa1f2aaddac62d3a5c3edeb93c0fc54b204db
AnarbayevA <azamat2000.com.com@gmail.com>
2022-02-26 18:46:33 +0300

    Ipoteka bank

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var elMoney = document.querySelector(".form__selector")
+var elNumber = document.querySelector(".form__number")
+var elForm = document.querySelector(".form")
+var elBtn = document.querySelector(".form__btn")
+var elHeader = document.querySelector(".header")
+
+elForm.addEventListener("submit", function(evt){
+    evt.preventDefault();
+    console.log(elNumber.value);
+    var result
+    
+    if(elMoney.value === "usd"){
+        result = elNumber.value * 10950;
+        console.log(result);
+    }
+
+    else if(elMoney.value ==="rub"){
+        result = elNumber.value * 145;
+        console.log(result);
+    }
+
+    else if(elMoney.value ==="evro"){
+        result = elNumber.value * 11200;
+        console.log(result);
+    }
+})
